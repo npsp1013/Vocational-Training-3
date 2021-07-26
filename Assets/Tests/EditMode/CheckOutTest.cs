7df4843ffb07436783e5d42cb6a8ec187ad6ec93
Jacky Ng <npsp2142@gmail.com>
2021-07-26 16:25:49 +0800

    Try Unity Unit Test

---
+++ Assets/Tests/EditMode/CheckOutTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class CheckOutTest
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void CheckOutTestSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+
+}
