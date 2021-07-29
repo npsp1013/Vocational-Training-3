71121be7356e44821aa77ef10ccc21e31917800e
Jacky Ng <npsp2142@gmail.com>
2021-07-29 16:15:41 +0800

    Client animation now play infinitely

--- Assets/Scripts/CheckOut/Customer.cs
+++ Assets/Scripts/CheckOut/Customer.cs
@@ -5,17 +5,23 @@ public class Customer : MonoBehaviour
 {
     [SerializeField] private float targetCardValue;
     [SerializeField] private OctopusCard card;
-
+    [SerializeField] private Animation animation;
     // Start is called before the first frame update
     void Awake()
     {
         card = gameObject.GetComponentInChildren<OctopusCard>();
+        animation = gameObject.GetComponentInChildren<Animation>();
     }
     public bool isCompleted()
     {
         if (card.Value < -100.0f) return false;
         float difference = (targetCardValue - card.Value);
         return difference < 0.01f && difference >= -0.01f;
+    }
+
+    private void Update()
+    {
+        animation.CrossFade("idle");
     }
 
     //TODO: Check whether the scanned items can fulfill the task
